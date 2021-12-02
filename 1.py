5edd364b4a89bb8ee5c45b3ce37ea92f2700eb9d
Sam Vanderstraeten <info@awesam.be>
2021-12-02 08:25:41 +0100

    II

--- 1.py
+++ 1.py
@@ -1,25 +1,19 @@
 file = open("input/1.sam", "r")
 lines = file.readlines()
+nums = [int(n) for n in lines]
 
 # Part I + fill list for part II
-nums = []
-prev = -1
 c = 0
-for line in lines:
-    num = int(line.strip())
-    if prev != -1 and num > prev:
+for n in range(1, len(nums)):
+    if nums[n-1] < nums[n]:
         c+=1
-    prev = num
-    nums.append(num)
 print(c)
-
 
 # Part II
 c = 0
-for n in range(0, len(nums)):
-    if n+2 < len(nums):
-        sw1 = sum(nums[n:n+3])
-        sw2 = sum(nums[n+1:n+4])
-        if sw2 > sw1:
-            c+=1
+for n in range(0, len(nums)-2):
+    sw1 = sum(nums[n:n+3])
+    sw2 = sum(nums[n+1:n+4])
+    if sw2 > sw1:
+        c+=1
 print(c)
--- 2.py
+++ 2.py
@@ -1,2 +1,32 @@
-file = open("test/2.sam", "r")
+file = open("input/2.sam", "r")
 lines = file.readlines()
+
+# Part I
+depth = 0
+h = 0
+for line in lines:
+    (instruction, amount) = line.split(" ")
+    amount = int(amount)
+    if instruction == "up":
+        depth -= amount
+    elif instruction == "down":
+        depth += amount
+    elif instruction == "forward":
+        h += amount
+print(depth*h)
+
+# Part II
+depth = 0
+h = 0
+aim = 0
+for line in lines:
+    (instruction, amount) = line.split(" ")
+    amount = int(amount)
+    if instruction == "up":
+        aim -= amount
+    elif instruction == "down":
+        aim +=  amount
+    elif instruction == "forward":
+        h += amount
+        depth += aim*amount
+print(depth*h)
