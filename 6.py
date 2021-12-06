9ff6cb55bd49fd4e6091e6d69db3ecd0e1acd096
Sam Vanderstraeten <info@awesam.be>
2021-12-06 08:21:14 +0100

    VI

--- 6.py
+++ 6.py
@@ -1,5 +1,8 @@
-file = open("test/6.sam", "r")
-lines = file.readlines()
-
-#data = [int(n) for n in lines]
-#data = [x.split(",") for x in lines]
+file = open("input/6.sam", "r")                     
+fish = [int(x) for x in file.readline().split(",")]
+num_for_state = [fish.count(i) for i in range(0,9)]
+for i in range(0, 256):
+    new_kids = num_for_state.pop(0)
+    num_for_state.append(new_kids)
+    num_for_state[6] += new_kids
+print(sum(num_for_state))
